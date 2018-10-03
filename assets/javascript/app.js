2513497690781ac0ab51fab9dc5645809cd42acb
FORTRESS <alexpetroski@gmail.com>
2018-10-03 07:25:12 -0400

    Added firebase and made full markup

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB-vRgWa_Qq5CLIHYs3ZM1RUBjmXhTJcnA",
+    authDomain: "train-scheduler-45881.firebaseapp.com",
+    databaseURL: "https://train-scheduler-45881.firebaseio.com",
+    projectId: "train-scheduler-45881",
+    storageBucket: "train-scheduler-45881.appspot.com",
+    messagingSenderId: "1031995942365"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
