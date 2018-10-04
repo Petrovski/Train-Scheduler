efe8fbe9468da32c6123b9be856d41f27eab6f47
FORTRESS <alexpetroski@gmail.com>
2018-10-04 08:19:06 -0400

    Basic functionality working, but times and rows still not right

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,3 +10,47 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
+
+$(document).ready(function () {
+    
+    $("#submit").on("click", function(){
+
+        event.preventDefault();
+
+        var train = $("#train").val().trim()
+        var destination = $("#destination").val().trim()
+        var firsttrain = $("#firsttrain").val().trim()
+        var frequency = $("#frequency").val().trim()
+        
+        console.log(train)
+        console.log(destination)
+        console.log(firsttrain)
+        console.log(frequency)
+
+        database.ref().push({
+            train: train,
+            destination: destination,
+            firsttrain: firsttrain,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+
+        database.ref().on("child_added", function (childSnapshot) {
+
+        var newTrain = $("<tr>")
+        $("#tables").append(newTrain);
+        $(newTrain).prepend($("<td>")).text(train);
+        $(newTrain).append($("<td>")).text(destination);
+        $(newTrain).append($("<td>")).text(firsttrain);
+        $(newTrain).append($("<td>")).text(frequency);
+
+            console.log(childSnapshot.val().train);
+            console.log(childSnapshot.val().destination);
+            console.log(childSnapshot.val().firsttrain);
+            console.log(childSnapshot.val().frequency);
+
+        }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+        });
+    });
+});
